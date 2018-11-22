f66be919ae3f757718c909c823ff0230ae47c472
Pablo Alonso <jackercito@gmail.com>
2018-11-22 13:04:17 +0100

    COMMIT ACTUALIZACION version: 0.0.0 fecha: 22/11/2018 comentario:  - Se hace lo mismo con el environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
+import { _environment } from './environment.base'
 
 export const environment = {
-  production: true,
+  production: _environment.production,
+  RETURN_URL: _environment.RETURN_URL,
+  CLIENT_ID: _environment.CLIENT_ID,
+  CLIENT_DOMAIN: _environment.CLIENT_DOMAIN,
+  RESPONSE_TYPE: _environment.RESPONSE_TYPE,
+  AUDIENCE: _environment.AUDIENCE,
+  REDIRECT: _environment.REDIRECT
 };
