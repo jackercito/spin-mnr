356293aa68069e67ed2714d3e2fcf4f36ab37c5e
Pablo Alonso <jackercito@gmail.com>
2018-11-26 11:29:29 +0100

    commit: 2 version: 1.0.1 fecha: 26/11/2018 comentario:  - Actualizacion de los espectrometos y las sondas

--- src/app/modulo-experimentos/componentes/crear-experimento/crear-experimento.component.ts
+++ src/app/modulo-experimentos/componentes/crear-experimento/crear-experimento.component.ts
@@ -8,10 +8,10 @@ import { Experimento } from '../../modelo/experimento.model'
 
 import { SnotifyService, SnotifyPosition } from 'ng-snotify';
 
-const ESPECTROMETROS = ['ESPECTROMETRO VARIAN MERCURY AS400', 'ESPECTRÓMETRO BRUKER AVANCE III / 500(Muestras Líquidas)', 'ESPECTRÓMETRO BRUKER AVANCE III / 500(Muestras sólidas)'];
+const ESPECTROMETROS = ['ESPECTROMETRO VARIAN MERCURY AS400', 'ESPECTRÓMETRO BRUKER AVANCE III / 500(Muestras Líquidas)', 'ESPECTRÓMETRO BRUKER AVANCE III / HD(Muestras sólidas)'];
 const SONDA_A = ['4NUC (5mm)', 'ATB (5mm)','BB (10mm)']
 const SONDA_B = ['PABBI (5mm)', 'PASEX (10mm)']
-const SONDA_C = ['SPRB400172_7164 (7,5mm)', 'SPRB400172_7423 (7,5mm)']
+const SONDA_C = ['SPRB400172_7164 (7,5mm)', 'SPRB400172_7423 (7,5mm)', 'H8906-20_007 (Triple Resonancia)', 'H13664_0016 (2,5mm)', 'H12138_0076 (Dobe Resonancia)', 'H13349_0014 (Baja frecuencia)']
 
 @Component({
   selector: 'app-crear-experimento',
