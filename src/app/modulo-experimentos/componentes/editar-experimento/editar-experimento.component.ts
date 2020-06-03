b0e8bf7ab166b905359f44c21eec0b4bc70bd116
Pablo Alonso <jackercito@gmail.com>
2020-06-03 12:42:17 +0200

    actualizacion de sondas (eliminacion de sonda BB 10 mm)

--- src/app/modulo-experimentos/componentes/editar-experimento/editar-experimento.component.ts
+++ src/app/modulo-experimentos/componentes/editar-experimento/editar-experimento.component.ts
@@ -9,7 +9,7 @@ import { Experimento } from '../../modelo/experimento.model'
 import { ExperimentosServiceService } from './../../services/experimentos-service.service';
 
 const ESPECTROMETROS = ['ESPECTROMETRO VARIAN MERCURY AS400', 'ESPECTRÓMETRO BRUKER AVANCE III / 500(Muestras Líquidas)', 'ESPECTRÓMETRO BRUKER AVANCE III / HD(Muestras sólidas)'];
-const SONDA_A = ['4NUC (5mm)', 'ATB (5mm)', 'BB (10mm)']
+const SONDA_A = ['4NUC (5mm)', 'ATB (5mm)']
 const SONDA_B = ['PABBI (5mm)', 'PASEX (10mm)']
 const SONDA_C = ['SPRB400172_7164 (7,5mm)', 'SPRB400172_7423 (7,5mm)', 'H8906-20_007 (Triple Resonancia)', 'H13664_0016 (2,5mm)', 'H12138_0076 (Dobe Resonancia)', 'H13349_0014 (Baja frecuencia)']
 
