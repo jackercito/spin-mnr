8b2bd380ac2ae45300e2ff4c80691fe68e429ce3
Pablo Alonso <jackercito@gmail.com>
2020-12-15 12:05:59 +0100

    pruebas

--- src/server/bd/mongodb.conexion.js
+++ src/server/bd/mongodb.conexion.js
@@ -6,8 +6,22 @@ const USER = process.env.MONGODB_USER;
 const PASSWORD = process.env.MONGODB_PASSWORD;
 const DB = process.env.MONGO_DB;
 
+var uri = "mongodb+srv://" + USER + ":" + PASSWORD + "@" + DB
+
+console.log(uri);
+
 //Conexion a la base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://" + USER + ":" + PASSWORD + "@" + DB, { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true }, (err, db) => {
+  if (err) console.log(err);
+  else {
+    var cursor = db.collection('experimentos').find();
+
+    cursor.each(function (err, doc) {
+      console.log(doc);
+    })
+  }
+}
+);
 
 exports.mongoose = mongoose;
