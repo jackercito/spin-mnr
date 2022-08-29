7b57d939849f6ffd8179c2c1b23811fe433f2f3e
Pablo Alonso <jackercito@gmail.com>
2022-08-29 14:16:20 +0200

    commit env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: false,
-  RETURN_URL: "https://app-spin.herokuapp.com/",
+  production: true,
+  RETURN_URL: "https://spin-mnr.vercel.app/",
   CLIENT_ID: "UAN7a6NXyx2VQcWgzRw1uHW9UPPSA8FS",
   CLIENT_DOMAIN: "jackercito.eu.auth0.com",
   RESPONSE_TYPE: "token id_token",
   AUDIENCE: "https://api.spin.jackercito/",
-  REDIRECT: "https://app-spin.herokuapp.com/callback",
+  REDIRECT: "https://spin-mnr.vercel.app/callback",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  RETURN_URL: "http://localhost:4200",
+  RETURN_URL: "https://spin-mnr.vercel.app/",
   CLIENT_ID: "UAN7a6NXyx2VQcWgzRw1uHW9UPPSA8FS",
   CLIENT_DOMAIN: "jackercito.eu.auth0.com",
   RESPONSE_TYPE: "token id_token",
   AUDIENCE: "https://api.spin.jackercito/",
-  REDIRECT: "http://localhost:4200/callback",
+  REDIRECT: "https://spin-mnr.vercel.app/callback",
 };
